eb37886eaf303dc6df643758872a57e8c0aa26aa
Crisandry Javier <crisandryj@gmail.com>
2022-01-27 18:39:13 -0500

    completed rps

--- rockpaper.js
+++ rockpaper.js
@@ -10,62 +10,105 @@ function computerPlay() {
  /* declare who beats who
     alert show winner 
  */
+
+    
+    const container = document.querySelector(`.container`)
+    
+    const content = document.createElement('div')
+    content.classList.add('content')
+    content.setAttribute('style', 'color: black; background: red'); 
+    container.appendChild(content); 
+
 function rps(playerSelection,computerSelection){
    let ps = playerSelection.toLowerCase();
    let cs = computerSelection.toLowerCase();
   
 
     if (ps === 'rock'& cs ==='scissors'){
-        console.log(` ${playerSelection} beats ${computerSelection} - Player Wins!`)
+       content.textContent = (` ${playerSelection} beats ${computerSelection} - Player Wins!`)
        return 'player';}
     else if (ps === 'paper'& cs==='rock'){
-        console.log(` ${playerSelection} beats ${computerSelection} - Player Wins!`)
+        content.textContent  = (` ${playerSelection} beats ${computerSelection} - Player Wins!`)
         return 'player';}      
     else if (ps === 'scissors'& cs==='paper'){
-        console.log(` ${playerSelection} beats ${computerSelection} - Player Wins!`)
+        content.textContent  = (` ${playerSelection} beats ${computerSelection} - Player Wins!`)
         return 'player';}   
 
     else if (ps === 'scissors'& cs==='rock'){
-        console.log(`${computerSelection} beats ${playerSelection}- Computer Wins!`)
+        content.textContent  =(`${computerSelection} beats ${playerSelection}- Computer Wins!`)
         return 'computer';}            
     else if (ps === 'rock'& cs ==='paper'){
-        console.log(`${computerSelection} beats ${playerSelection}- Computer Wins!`)
+        content.textContent  = (`${computerSelection} beats ${playerSelection}- Computer Wins!`)
         return 'computer';}     
     else if (ps === 'paper'& cs==='scissors'){
-        console.log(`${computerSelection} beats ${playerSelection}- Computer Wins!`)
+        content.textContent  = (`${computerSelection} beats ${playerSelection}- Computer Wins!`)
         return 'computer';}   
 
     else if (ps === 'paper'& cs==='paper'){
+        content.textContent  = "Draw"
         return 'draw';}  
     else if (ps === 'rock'& cs==='rock'){
+        content.textContent  = "Draw"
         return 'draw';}  
     else if (ps === 'scissors'& cs==='scissors'){
+        content.textContent  = "Draw"
         return 'draw';}  
 
 
     else {alert("Incorrect entry")}    
     };
 
-    let computer = 0
-    let player = 0
-    let draw = 0
+    let computer = 0;
+    let player = 0;
+    let draw = 0;
 
-    function round() {
-       let result = rps(prompt("Rock, Paper or Scissors?"),computerPlay())
-       return result;
-        
+   
+    const playerResult = document.querySelector('.playerResult')
+    const computerResult = document.querySelector('.computerResult')
+    const drawResult = document.querySelector('.drawResult')
+    const annouceWinner = document.querySelector('#annoucement')
+    annouceWinner.style.cssText = 'color: white; background: black; font-size:20px;'    
+    
+    
+    function playRound() {
+       let result = rps(this.value,computerPlay())
+
+       if (result === 'player'){player+=1}
+       else if (result === 'computer'){computer+=1}
+       else {draw+=1};
+       console.log(result)
+       console.log(player)
+       playerResult.textContent = `${player}`
+       computerResult.textContent = `${computer}`
+       drawResult.textContent = `${draw}`
+
+       if (player === 5) {annouceWinner.textContent = "PLAYER WINS!!!"}
+       else if (computer === 5) {annouceWinner.textContent = "COMPUTER WINS!!!"}
     }
 
-function game() {
-for (let i = 0; i<5; i++){
-        let result = round()
-        if(result==='player') {player+=1} 
-        else if ('computer') {computer+=1}
-        else {draw+=1}
-}
-             
-  return `Computer: ${computer} Player ${player} Draws ${draw}`
-    }
+    const btn = document.querySelectorAll(".btn")
+    
+
+    btn.forEach((btn) => {
+        btn.addEventListener('click', playRound)
+   
+    })
+    
+  
+
+
+
+   
+    
+  
 
 
   
+
+    
+
+
+    
+
+    
+    
